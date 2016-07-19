7ae0fc4f89e49d884bc382f453a20be2d097eacb
Yi Chiang and russ vetsper <yi+russ@>
2016-07-19 11:03:35 -0700

    add output for html

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -11,6 +11,10 @@ namespace AnagramList
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
+      Get["/result"] = _ => {
+        Anagram myAnagram=new Anagram(Request.Query["new-ourWord"],Request.Query["new-userWord"]);
+        return View["result.cshtml",myAnagram];
+      };
 
     }
   }
